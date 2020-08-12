12076c58c5cbc4768d4643dcef6130266060bb46
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-08-12 20:15:01 +0200

    log1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+#[macro_use]
+extern crate log;
+
+pub fn init_logging() {
+    simplelog::CombinedLogger::init(vec![
+        simplelog::TermLogger::new(
+            simplelog::LevelFilter::Trace,
+            simplelog::Config::default(),
+            simplelog::TerminalMode::Mixed,
+        ),
+        simplelog::WriteLogger::new(
+            simplelog::LevelFilter::Warn,
+            simplelog::Config::default(),
+            std::fs::File::create("res/logs/warn.log").unwrap(),
+        ),
+    ])
+    .unwrap();
+}
+
 pub fn hello_hazel() {
-    println!("Hello Hazel!!");
+    error!("Error Hazel!!");
+    warn!("Warn Hazel!!");
+    info!("Info Hazel!!");
+    debug!("Debug Hazel!!");
+    trace!("Trace Hazel!!");
 }
